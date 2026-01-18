c87812e8dc5ddf5a8b4f6cbc052a210f74245138
ExploitEngineer <revilwhoami0098@gmail.com>
2026-01-18 22:38:56 +0500

    feat(roadmap): add hello world in Go

---
+++ roadmap/01-Introduction-to-Go/05-hello-world.go
@@ -0,0 +1,9 @@
+// Traditional first program demonstrating basic structure; `package main`, importing `fmt`, and `main()` functoin using `fmt.Println()`. Teaches Go syntax, compilation, execution, and verifies development environment setup. Entry point for learning Go.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
